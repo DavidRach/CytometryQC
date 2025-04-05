7103248e44e7c387c3288c001774df9d7a993703
David Rach <drach@som.umaryland.edu>
2025-04-04 20:41:46 -0400

    A good portion of the add instrument script. Tiring out for evening.

--- R/AddInstruments.R
+++ R/AddInstruments.R
@@ -356,8 +356,152 @@ RCV_Display <- function(uv=uv, violet=violet,
   return(AllCombined)
 }
 
-AddInstrumentScript <- function(){
-message("In preparation")
+AddInstrumentScript <- function(name, outpath){
+
+filename <- paste0("TheScript_", name, ".qmd")
+StorageLocation <- file.path(outpath, filename)
+
+TheCytekbioExport <- file.path("C:", "CytekbioExport")
+TheSetupFolder <- file.path(TheCytekbioExport, "Setup")
+TheFCSFolder <- file.path("D:", "Aurora 3_FCS Files", "Experiments", "Flow Core")
+
+FirstChunk <- 'library(stringr)
+library(purrr)
+
+# Find out current date
+Today <- Sys.Date()
+Today <- as.Date(Today)
+
+WorkingDirectory <- file.path(CytometryQC::OperatingSystemCheck(), "InstrumentQC2")
+'
+  
+SecondChunk <- sprintf('setwd(WorkingDirectory)
+
+# Check for Flag Files
+AnyFlags <- list.files(WorkingDirectory, pattern="Flag.csv", full.names=TRUE)
+
+if (length(AnyFlags) == 0){
+
+# Git Pull
+RepositoryPath <- WorkingDirectory
+TheRepo <- git2r::repository(RepositoryPath)
+git2r::pull(TheRepo)
+
+# Locating Archive Folder
+Instrument <- "%s"
+', name)
+
+ThirdChunk <- '
+MainFolder <- file.path(WorkingDirectory, "data")
+WorkingFolder <- file.path(WorkingDirectory, "data", Instrument)
+StorageFolder <- file.path(WorkingFolder, "Archive")
+
+# Gains
+Gains <- list.files(StorageFolder, pattern="Archived", full.names=TRUE)
+Gains <- read.csv(Gains[1], check.names = FALSE)
+LastGainItem <- Gains |> dplyr::slice(1) |> dplyr::pull(DateTime)
+LastGainItem <- lubridate::ymd_hms(LastGainItem)
+LastGainItem <- as.Date(LastGainItem)
+PotentialGainDays <- seq.Date(from = LastGainItem, to = Today, by = "day")
+GainRemoveIndex <- which(PotentialGainDays == LastGainItem)
+PotentialGainDays <- PotentialGainDays[-GainRemoveIndex]
+
+# MFIs
+MFIs <- list.files(StorageFolder, pattern="Bead", full.names=TRUE)
+MFIs <- read.csv(MFIs[1], check.names=FALSE)
+LastMFIItem <- MFIs |> dplyr::slice(1) |> dplyr::pull(DateTime)
+LastMFIItem <- lubridate::ymd_hms(LastMFIItem)
+LastMFIItem <- as.Date(LastMFIItem)
+PotentialMFIDays <- seq.Date(from = LastMFIItem, to = Today, by = "day")
+MFIRemoveIndex <- which(PotentialMFIDays == LastMFIItem)
+PotentialMFIDays <- PotentialMFIDays[-MFIRemoveIndex]
+  
+# Usage
+Apps <- list.files(StorageFolder, pattern="Application", full.names=TRUE)
+Apps <- read.csv(Apps[1], check.names=FALSE)
+LastAppsItem <- Apps |> dplyr::slice(1) |> dplyr::pull(DateTime)
+LastAppsItem <- lubridate::ymd_hms(LastAppsItem)
+LastAppsItem <- as.Date(LastAppsItem)
+PotentialAppsDays <- seq.Date(from = LastAppsItem, to = Today, by = "day")
+AppsRemoveIndex <- which(PotentialAppsDays == LastAppsItem)
+PotentialAppsDays <- PotentialAppsDays[-AppsRemoveIndex]
+'
+  
+FourthChunk <- sprintf('if (!length(PotentialGainDays) == 0){
+
+SetupFolder <- %s
+TheSetupFiles <- list.files(SetupFolder, pattern="DailyQC", full.names=TRUE)
+Dates <- as.character(PotentialGainDays)
+Dates <- gsub("-", "", Dates)
+GainMatches <- TheSetupFiles[str_detect(TheSetupFiles, str_c(Dates, collapse = "|"))]
+
+if (!length(GainMatches) == 0){
+file.copy(GainMatches, WorkingFolder)
+walk(.x=Instrument, .f=Luciernaga:::DailyQCParse, MainFolder=MainFolder)
+}
+} else {message("QC data has already been transferred")
+  GainMatches <- NULL
+}
+
+', TheSetupFolder)
+  
+FifthChunk <- sprintf('if (!length(PotentialMFIDays) == 0){
+FCSFolder <-  %s
+MonthStyle <- format(Today, "%Y-%m")
+MonthFolder <- paste0("QC ", MonthStyle)
+MonthFolder <- file.path(FCSFolder, MonthFolder)
+TheFCSFiles <- list.files(MonthFolder, pattern="fcs", full.names=TRUE, recursive=TRUE)
+days <- format(PotentialMFIDays, "%d")
+MFIMatches <- TheFCSFiles[str_detect(basename(TheFCSFiles), str_c(days, collapse = "|"))]
+
+if (!length(MFIMatches) == 0){
+file.copy(MFIMatches, WorkingFolder)
+walk(.x=Instrument, .f=Luciernaga:::QCBeadParse, MainFolder=MainFolder)
+}
+} else {message("QC data has already been transferred")
+  MFIMatches <- NULL
+}
+', TheFCSFolder)
+
+SixChunk <- sprint('if (!length(PotentialAppsDays) == 0){
+    SetupFolder <- %s
+    TheSetupFiles <- list.files(SetupFolder, pattern="Application", full.names=TRUE)
+    MonthStyle <- format(Today, "%Y-%m")
+    MonthStyle <- sub("([0-9]{4})-([0-9]{2})", "\\2-\\1", MonthStyle)
+    MonthStyle <- gsub("-", " ", MonthStyle)
+    MonthStyle <- paste0(MonthStyle, ".txt")
+  
+    AppMatches <- TheSetupFiles[str_detect(TheSetupFiles, str_c(MonthStyle, collapse = "|"))]
+    
+    if (!length(AppMatches) == 0){
+
+      if (any(length(GainMatches)|length(MFIMatches) > 0)){
+      file.copy(AppMatches, WorkingFolder)
+      walk(.x=Instrument, .f=Luciernaga:::AppQCParse, MainFolder=MainFolder)
+      }
+      }
+} else {message("QC data has already been transferred")
+    AppMatches <- NULL
+}
+', TheCytekbioExport)
+
+SeventhChunk <- 'if (any(length(PotentialGainDays)|length(PotentialMFIDays)|length(PotentialAppsDays) > 0)){
+  
+  if (any(length(GainMatches)|length(MFIMatches) > 0)){
+  # Stage to Git
+    git2r::add(TheRepo, "*")
+  
+  TheCommitMessage <- paste0("Update for ", Instrument, " on ", Today)
+  git2r::commit(TheRepo, message = TheCommitMessage)
+  cred <- git2r::cred_token(token = "GITHUB_PAT")
+  git2r::push(TheRepo, credentials = cred)
+  message("Done ", Today)
+  } else {message("No files to process ", Today)}
+} else {message("No files to process ", Today)}
+} else {message("Automation Skipped ", Today)}
+ '
+ cat(FirstChunk, SecondChunk, ThirdChunk, FourthChunk, FifthChunk, SixChunk,
+  SeventhChunk, file = StorageLocation)
 }
 
 AddInstrumentQMD <- function(name="5L", outpath="/home/david/Desktop", organization="UMGCC FCSS",
@@ -625,3 +769,4 @@ This dashboard was created with [Quarto](https://quarto.org/) using [CytometryQC
 cat(Section1, SectionMFI, SectionGain, SectionRCV, SectionPDF, Section2, file = StorageLocation)
 
 }
+  
