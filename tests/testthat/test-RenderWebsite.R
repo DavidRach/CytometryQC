707dbc9a7460c8277c1440db6e966d942bff8324
David Rach <drach@som.umaryland.edu>
2026-03-08 12:16:47 -0400

    Success linking vdiffr outputs, needed to manually create the snaps and specific folder to get the files to populate and carry over.

--- tests/testthat/test-RenderWebsite.R
+++ tests/testthat/test-RenderWebsite.R
@@ -1,46 +1,63 @@
-test_that("FolderSetup Creates Required Infrastructure", {
+test_that("Render Website results in consistent webpage", {
+
   tmp <- withr::local_tempdir(pattern = "CytometryQC")
   withr::local_dir(tmp)
 
   FolderSetup(
-  SetUpGit = FALSE,
-  organization_name = "UMGCC FCSS",
-  organization_website = "https://www.medschool.umaryland.edu/cibr/core/umgccc_flow/",
-  githubusername = "umgccfcss",
-  institution_name = "University of Maryland, Baltimore",
-  AlternateDirectory = tmp
+    SetUpGit = FALSE,
+    organization_name = "UMGCC FCSS",
+    organization_website = "https://www.medschool.umaryland.edu/cibr/core/umgccc_flow/",
+    githubusername = "umgccfcss",
+    institution_name = "University of Maryland, Baltimore",
+    AlternateDirectory = tmp
   )
 
-  Folder <- list.files(tmp, pattern="InstrumentQC", full.names=TRUE)
+  Folder <- list.files(tmp, pattern = "InstrumentQC", full.names = TRUE)
 
-  setwd(Folder)
+  withr::with_dir(Folder, {system("quarto render")})
 
-  RenderWebsite(AlternateRender = "Yes")
+  DocsFolder <- file.path(Folder, "docs")
+
+  files <- list.files(DocsFolder, full.names=TRUE)
 
   pages <- c(
     "Data.html",
     "help.html",
-    "historical.html",
-    "Instrumental.html",
+    "Historical.html",
+    "Instrument.html",
     "Miscellaneous.html"
   )
 
   for (p in pages) {
-
-    html <- file.path("docs", p)
-    expect_true(file.exists(html))
-
-    png <- tempfile(fileext = ".png")
+    html <- file.path(DocsFolder, p)
+    snapshot_name <- paste0(tools::file_path_sans_ext(p), ".png")
+    png <- file.path(tmp, snapshot_name)
 
     webshot2::webshot(
-      url = paste0("file:///", normalizePath(html)),
-      file = png,
+      url    = paste0("file:///", normalizePath(html)),
+      file   = png,
       vwidth = 1400,
       vheight = 1000,
-      delay = 0.5
+      delay  = 0.5
     )
-
-    testthat::expect_snapshot_file(png)
   }
+
+  PNGs <- list.files(tmp, ".png", full.names=TRUE)
+
+  for (png_file in PNGs) {
+
+  name <- tools::file_path_sans_ext(basename(png_file))
+
+  img <- png::readPNG(png_file)
+
+  fig <- function() {
+    grid::grid.raster(img)
+  }
+
+  vdiffr::expect_doppelganger(
+    title = paste("website-", name),
+    fig = fig
+  )
+}
 
 })
